d493a7e5bb98bb9ef2be8852958cb9e904d62627
Janice Luo <janiceluo@Janices-MacBook-Pro.local>
2019-11-05 18:32:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,36 @@ let shoppingCartItems = [
     description: 'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
     price: 100
   },
+  {
+    item: 'Spam',
+    image: 'https://thumbs-prod.si-cdn.com/H7aVwwcff-NyQDH0opXs7a6BDUg=/800x600/filters:no_upscale()/https://public-media.si-cdn.com/filer/a3/a5/a3a5e93c-0fd2-4ee7-b2ec-04616b1727d1/kq4q5h7f-1498751693.jpg',
+    description: 'Spam can last for more than that 3 month long trip',
+    price: 30
+  },
+  {
+    item: 'Jacket',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/614hxizXuqL._UY606_.jpg',
+    description: 'Nice warm jacket because it will be cold AF',
+    price: 200
+  },
+  {
+    item: 'Rain Boots',
+    image: 'https://di2ponv0v5otw.cloudfront.net/posts/2019/07/13/5d298de62f4831661403bc64/s_5d298e0b8d653d3b85bacc86.jpg',
+    description: 'Rain boots for the rain',
+    price: 50
+  },
+  {
+    item: 'Chopsticks',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81XEN3w9iJL._SX425_.jpg',
+    description: "Chopsticks because you will need to eat",
+    price: 10
+  },
+  {
+    item: 'Metal Straw',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71vIIgE4BRL._SX466_.jpg',
+    description: "Save the turtles",
+    price: 15
+  }
 ]
 
 function App() {
@@ -23,15 +53,25 @@ function App() {
 function ShoppingCart() {
   return(
     <div className="cart">
-      <Product />
+      {shoppingCartItems.map(Product)}
     </div>
   );
 }
 
-function Product() {
+
+function Product(props) {
   return(
     <div className="product">
-      <img src={shoppingCartItems[0].image} width="100"/>
+      <div className="item-name">
+        <h1> {props.item} </h1>
+      </div>
+      <div className="item-image">
+        <img src={props.image} width="100"/>  
+      </div>
+      <div className="item-description">
+        <p> {props.description}</p>
+        <p> {props.price} </p>
+      </div>
     </div>
   )
 }
